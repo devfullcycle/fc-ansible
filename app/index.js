08bc06aca7cea81dcf0629667de43f72b870a280
Wesley Willians <wesleywillians@gmail.com>
2022-02-11 11:43:40 -0500

    first

---
+++ app/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3002;
+
+app.get('/', (req, res) => res.send('Hello Full Cycle'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
